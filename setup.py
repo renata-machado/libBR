de6a868532bc9e682fed81e2ad99920673bc5679
Renata Machado Barreto Braga <20251SI026@cefetes.br>
2025-05-22 14:01:00 -0300

    atualizando versão no setup tirando o tkinter (install_requires=[]) 0.1.3

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='libbr',
-    version='0.1.2',
+    version='0.1.3',
     author='Renata Machado Barreto Braga',
     description='Uma biblioteca gráfica simples e brasileira, estilo PySimpleGUI.',
     packages=find_packages(),
-    install_requires=[
-        'tkinter',  # pode ser omitido; já vem com o Python padrão
-    ],
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
